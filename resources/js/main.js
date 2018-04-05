2fb5f6b0518e4e4aa8844c8790aa1a80f53efb95
nnngu <654015592@qq.com>
2018-04-05 23:13:26 +0800

    提交代码

---
+++ resources/js/main.js
@@ -0,0 +1,26 @@
+$(function () {
+    $("#btn1").attr("checked","checked");
+    $("#btn_search").click(function () {
+        var site = $("input[name='site']:checked").val();
+        var keyword = $("#keyword").val();
+
+        if (site == "种子搜") {
+            // 去"种子搜"收集磁力链接
+            $("#result").attr('src', "https://www.zhongziso.com/list/" + keyword + "/1");
+        }
+        if (site == "磁力吧") {
+            // 去"磁力吧"收集磁力链接
+            $("#result").attr('src', "https://www.ciliba.org/s/"+ keyword +".html");
+        }
+        if (site == "cililiana") {
+            // 去"cililiana"收集磁力链接
+            $("#result").attr('src', "http://cililianc.com/list/"+ keyword +"/1.html");
+        }
+        if (site == "屌丝搜") {
+            // 去"屌丝搜"收集磁力链接
+            $("#result").attr('src', "http://www.diaosisou.org/list/"+ keyword +"/1");
+        }
+    });
+
+});
+
